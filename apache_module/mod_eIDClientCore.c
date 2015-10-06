b997d653fc2c571e78437b898fc7849dd90d6f54
Erik Nellessen <erik.nellessen@informatik.hu-berlin.de>
2015-10-06 13:59:35 +0200

    Added data structure for eID results. Added example of working with the result.

--- apache_module/mod_eIDClientCore.c
+++ apache_module/mod_eIDClientCore.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "mod_eIDClientCore.h"
 #define BUFFER_SIZE 1024
 
 char Parser[] = "";
@@ -38,10 +39,18 @@ static void register_hooks(apr_pool_t *pool)
     ap_hook_handler(eIDClientCore_handler, NULL, NULL, APR_HOOK_LAST);
 }
 
+char allowedFirstNames [] = "Erik";
+int showSecret(char *firstName){
+	if(firstName != NULL && strcasestr(allowedFirstNames, firstName) != NULL){
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
 /* The handler function for our module.
  * This is where all the fun happens!
  */
-
 static int eIDClientCore_handler(request_rec *r)
 {
 	/* First off, we need to check if this is a call for the "eIDClientCore" handler.
@@ -68,9 +77,66 @@ static int eIDClientCore_handler(request_rec *r)
 			pos += size;
 		}
 	}
-
-	if(data) {
-		ap_rputs(data, r);
+	
+	int eIDCCReturnValue = pclose(pipe);
+	eIDInformation mEIDInformation = {0};
+	//Get the data
+	if(data && eIDCCReturnValue == 0) {
+		//Fill data structure
+		char *foundString = data, **valueToFill, *startCopy;
+		char searchString[] = ":\n";
+		int i, nextLineBreak;
+		for(i = 0; ; i++){
+			//Set pointer to correct value in data structure
+			switch(i){
+				case 0: valueToFill = &mEIDInformation.title;
+					break;
+				case 1: valueToFill = &mEIDInformation.artistName;
+					break;
+				case 2: valueToFill = &mEIDInformation.firstName;
+					break;
+				case 3: valueToFill = &mEIDInformation.lastName;
+					break;
+				case 4: valueToFill = &mEIDInformation.birthName;
+					break;
+				case 5: valueToFill = &mEIDInformation.placeOfResidence;
+					break;
+				case 6: valueToFill = &mEIDInformation.placeOfBirth;
+					break;
+				case 7: valueToFill = &mEIDInformation.documentType;
+					break;
+				case 8: valueToFill = &mEIDInformation.issuingState;
+					break;
+				case 9: valueToFill = &mEIDInformation.nationality;
+					break;
+				case 10: valueToFill = &mEIDInformation.residencePermit;
+					break;
+				default: break;
+			}
+			
+			foundString = strstr(foundString, searchString);
+			if(foundString == NULL)
+				break;
+			foundString += strlen(searchString);
+			
+			if(*foundString == '\n'){
+				*valueToFill = NULL;
+			} else {
+				startCopy = foundString;
+				nextLineBreak = strcspn(foundString, "\n");
+				*valueToFill = malloc(sizeof(char) * (nextLineBreak + 1));
+				strncpy(*valueToFill, startCopy, nextLineBreak);
+				char *string = *valueToFill;
+				string[nextLineBreak] = '\0';
+			}
+		}
+	}
+	
+	if(showSecret(mEIDInformation.firstName) == 0){
+		ap_rputs("The secret is: Soylent Green is people!\n", r);
+	} else {
+		if(data)
+			ap_rputs(data, r);
 	}
 	
 	free(data);
---
+++ apache_module/mod_eIDClientCore.h
@@ -0,0 +1,15 @@
+typedef struct eIDInformationStruct {
+	char *title;
+	char *artistName;
+	char *firstName;
+	char *lastName;
+	char *birthName;
+	char *placeOfResidence;
+	char *placeOfBirth;
+	char *dateOfBirth;
+	char *documentType;
+	char *issuingState;
+	char *nationality;
+	char *residencePermit;
+} eIDInformation;
+
